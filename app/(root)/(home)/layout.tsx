356643df6e8766cfb48cd94952585c3925908e9e
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-04-12 16:27:09 +0530

    added Title and icon to the layout

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,17 @@
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+
+export const metadata: Metadata = {
+    title: "Zoom by Kunal",
+    description: "Video Calling App",
+    icons : {
+      icon : '/icons/logo.svg'
+    }
+  };
+
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
     return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,15 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+
+export const metadata: Metadata = {
+    title: "Zoom by Kunal",
+    description: "Video Calling App",
+    icons : {
+      icon : '/icons/logo.svg'
+    }
+  };
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
