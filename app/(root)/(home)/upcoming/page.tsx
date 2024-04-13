e8ea9e18c12b5acc3ab3467253a28ac07fe6a19a
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-04-13 14:56:59 +0530

    added useGetCalls hook

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,3 +1,4 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Upcoming = () => {
@@ -6,6 +7,8 @@ const Upcoming = () => {
       <h1 className='text-3xl font-bold'>
         Upcoming
       </h1>
+
+      <CallList type='upcoming' />
     </section>
   )
 }
---
+++ components/CallList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallList = ({type} : {type : 'ended' | 'upcoming' | 'recordings'}) => {
+  return (
+    <div>CallList</div>
+  )
+}
+
+export default CallList
